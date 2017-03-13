0c7b3538d9b05852837800e61e02a7283d6a4226
RatanShreshtha <ratan.shreshtha@gmail.com>
2017-03-13 16:38:12 +0530

    Structured the directory.

--- fractals/mandelbrot-set.py
+++ fractals/mandelbrot-set.py
@@ -11,6 +11,7 @@ def m(a):
             return n
     return NaN
 
+
 X = arange(-2, .5, .002)
 Y = arange(-1,  1, .002)
 Z = zeros((len(Y), len(X)))
@@ -18,11 +19,12 @@ Z = zeros((len(Y), len(X)))
 for iy, y in enumerate(Y):
     print (iy, "of", len(Y))
     for ix, x in enumerate(X):
-        Z[iy,ix] = m(x + 1j * y)
+        Z[iy, ix] = m(x + 1j * y)
 
-imshow(Z, cmap = plt.cm.prism, interpolation = 'none', extent = (X.min(), X.max(), Y.min(), Y.max()))
+imshow(Z, cmap=plt.cm.prism, interpolation='none',
+       extent=(X.min(), X.max(), Y.min(), Y.max()))
+
 xlabel("Re(c)")
 ylabel("Im(c)")
 savefig("mandelbrot_python.svg")
 show()
-
